303e6d646bcbdd0f57a5f0254205185dbab3e67a
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-01 19:43:30 -0500

    Change background image in page component
    Updated background image URL to a new source.

--- app/page.jsx
+++ app/page.jsx
@@ -61,7 +61,7 @@ export default function Home() {
         justifyContent: "center",
         alignItems: "center",
         textAlign: "center",
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('https://images.unsplash.com/photo-1614164185128-e4ec99c436d7?')`, // Imagen de relojería de lujo
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('https://cdn.pixabay.com/photo/2015/09/24/20/36/watch-956291_1280.jpg')`, // Imagen de relojería de lujo
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundAttachment: "fixed", // Efecto Parallax elegante
