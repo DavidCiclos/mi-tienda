974ca2392095f82fc7209b1cb4c0288da19e8806
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-07 21:19:30 -0500

    Add product catalog to productos.js

--- app/productos.js
+++ app/productos.js
@@ -1,1 +1,24 @@
+// productos.js - Catálogo de APEX TIME
+export const listaProductos = [
+  {
+    id: 1,
+    name: "Daytona S-Clon Gold",
+    brand: "Rolex",
+    line: "SUIZOS-S", // Estilo dorado/premium
+    price: 3450000, 
+    images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1777254410/11647_ppetcc.jpg"],
+    description: "Maquinaria suiza, acero 904L, cristal de zafiro.",
+    warranty: "2 años"
+  },
+  {
+    id: 2,
+    name: "Submariner Black Dial",
+    brand: "Rolex",
+    line: "NORMAL", // Estilo estándar/acero
+    price: 1850000, 
+    images: ["AQUÍ_PEGA_OTRO_LINK_DE_CLOUDINARY"],
+    description: "Movimiento automático, bisel cerámico, resistente al agua.",
+    warranty: "1 año"
+  }
+];
 
