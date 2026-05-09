3f7974acea6a22e1e04637f08672fbf891e7990a
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-08 23:40:00 -0500

    Update product image URL in productos.js

--- app/productos.js
+++ app/productos.js
@@ -1,4 +1,4 @@
-// productos.js - Catálogo de APEX TIME
+ // productos.js - Catálogo de APEX TIME
 export const listaProductos = [
   {
     id: 1,
@@ -17,7 +17,7 @@ export const listaProductos = [
     line: "SUIZOS-S",
     price: 1850000,
     images: [
-      "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_hglc9b.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana.jpg",
       "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430.jpg",
     ],
     description: "Movimiento automático, bisel cerámico, resistente al agua.",
