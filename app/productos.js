70ca1baa43e6d31bc5ec4cb39f9981da104ab147
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-08 22:43:25 -0500

    Update product images in productos.js

--- app/productos.js
+++ app/productos.js
@@ -17,9 +17,9 @@ export const listaProductos = [
     line: "SUIZOS-S",
     price: 1850000,
     images: [
-      "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_ha.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_hglc9b.jpg",
       "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430.jpg",
-      "https://res.cloudinary.com/debewjkqh/image/upload/v1777254410/11647_ppetcc.jpg" 
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430_vpmq2q.jpg" 
     ],
     description: "Movimiento automático, bisel cerámico, resistente al agua.",
     warranty: "1 año"
