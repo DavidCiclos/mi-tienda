0de449939f9744184859fca44eb8038a8392801d
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-08 22:37:08 -0500

    Enhance Submariner Black Dial product entry
    Updated product details for Submariner Black Dial, including additional images.

--- app/productos.js
+++ app/productos.js
@@ -11,14 +11,14 @@ export const listaProductos = [
     warranty: "2 años"
   },
   {
-    id: 2,
-    name: "Submariner Black Dial",
-    brand: "ROLEX",
-    line: "SUIZOS-S", // Estilo estándar/acero
-    price: 1850000, 
-    images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_hglc9b.jpg"],
-    description: "Movimiento automático, bisel cerámico, resistente al agua.",
-    warranty: "2 años"
-  }
-];
-
+  id: 2,
+  name: "Submariner Black Dial",
+  // ... resto de datos
+  images: [
+    "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_hglc9b.jpg", // Esta es la que se ve primero
+    "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430_vpmq2q.jpg",   // Esta crea el segundo cuadro
+    "LINK_FOTO_3_DETALLE"    // Esta crea el tercer cuadro
+  ],
+  // ...
+},
+  
