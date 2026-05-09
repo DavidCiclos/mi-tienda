95f706632c341fa09344c78c341ef3fc1de08867
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-08 22:40:06 -0500

    Update product details for Daytona and Datejust

--- app/productos.js
+++ app/productos.js
@@ -4,21 +4,24 @@ export const listaProductos = [
     id: 1,
     name: "Daytona S-Clon Gold",
     brand: "ROLEX",
-    line: "SUIZOS-S", // Estilo dorado/premium
-    price: 3450000, 
+    line: "SUIZOS-S",
+    price: 3450000,
     images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1777254410/11647_ppetcc.jpg"],
     description: "Maquinaria suiza, acero 904L, cristal de zafiro.",
     warranty: "2 años"
   },
   {
-  id: 2,
-  name: "Submariner Black Dial",
-  // ... resto de datos
-  images: [
-    "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_hglc9b.jpg", // Esta es la que se ve primero
-    "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430_vpmq2q.jpg",   // Esta crea el segundo cuadro
-    "LINK_FOTO_3_DETALLE"    // Esta crea el tercer cuadro
-  ],
-  // ...
-},
-  
+    id: 2,
+    name: "Datejust",
+    brand: "ROLEX",
+    line: "SUIZOS-S",
+    price: 1850000,
+    images: [
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_ha.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1777254410/11647_ppetcc.jpg" 
+    ],
+    description: "Movimiento automático, bisel cerámico, resistente al agua.",
+    warranty: "1 año"
+  }
+];
