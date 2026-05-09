52da6d70f51becef6ffdbb2bb4790e14b8c256f5
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-08 19:58:16 -0500

    Fix typo in product line name for Submariner

--- app/productos.js
+++ app/productos.js
@@ -14,7 +14,7 @@ export const listaProductos = [
     id: 2,
     name: "Submariner Black Dial",
     brand: "ROLEX",
-    line: "SUISOS-S", // Estilo estándar/acero
+    line: "SUIZOS-S", // Estilo estándar/acero
     price: 1850000, 
     images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_hglc9b.jpg"],
     description: "Movimiento automático, bisel cerámico, resistente al agua.",
