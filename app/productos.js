29f27354be6f68860f449bdbe8788c9c38717711
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-08 22:44:55 -0500

    Remove duplicate image URL from productos.js

--- app/productos.js
+++ app/productos.js
@@ -18,8 +18,7 @@ export const listaProductos = [
     price: 1850000,
     images: [
       "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_hglc9b.jpg",
-      "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430.jpg",
-      "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430_vpmq2q.jpg" 
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430.jpg"
     ],
     description: "Movimiento automático, bisel cerámico, resistente al agua.",
     warranty: "1 año"
