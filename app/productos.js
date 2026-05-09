f212e9738f749087d9939c8b9eea69c510b96d93
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-08 19:55:55 -0500

    Update product details for Submariner watch

--- app/productos.js
+++ app/productos.js
@@ -13,12 +13,12 @@ export const listaProductos = [
   {
     id: 2,
     name: "Submariner Black Dial",
-    brand: "Rolex",
-    line: "NORMAL", // Estilo estándar/acero
+    brand: "ROLEX",
+    line: "SUISOS-S", // Estilo estándar/acero
     price: 1850000, 
-    images: ["AQUÍ_PEGA_OTRO_LINK_DE_CLOUDINARY"],
+    images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_hglc9b.jpg"],
     description: "Movimiento automático, bisel cerámico, resistente al agua.",
-    warranty: "1 año"
+    warranty: "2 años"
   }
 ];
 
