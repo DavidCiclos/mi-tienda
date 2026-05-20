8ed9ae731a80fd91dd2eb08bde93405504c43480
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-20 10:52:05 -0500

    Update product image URL for Daytona S-Clon Gold

--- app/productos.js
+++ app/productos.js
@@ -6,7 +6,7 @@ export const listaProductos = [
     brand: "ROLEX",
     line: "SUIZOS-S",
     price: 0,
-    images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1777254410/11647_ppetcc.jpg"],
+    images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1779291658/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_1_2_ejumav.jpg"],
     description: "Maquinaria suiza, acero 904L, cristal de zafiro.",
     warranty: "2 años"
   },
