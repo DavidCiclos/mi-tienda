e669b6d92aa15e3045907fe3480ed911d6a500db
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-20 11:46:33 -0500

    Fix image array formatting in productos.js

--- app/productos.js
+++ app/productos.js
@@ -6,7 +6,8 @@ export const listaProductos = [
     brand: "ROLEX",
     line: "SUIZOS-S",
     price: 0,
-    images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1779291658/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_1_2_ejumav.jpg"
+    images: [
+    "https://res.cloudinary.com/debewjkqh/image/upload/v1779291658/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_1_2_ejumav.jpg"
     "https://res.cloudinary.com/debewjkqh/image/upload/v1779291679/20251129_153935_f3xmcj.jpg 
      ],        
     description: "Maquinaria suiza, acero 904L, cristal de zafiro.",
@@ -18,7 +19,8 @@ export const listaProductos = [
     brand: "ROLEX",
     line: "SUIZOS-S",
     price: 1850000,
-    images: ["https://res.cloudinary.com/debewjkqh/image/upload/v1779292975/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_lafh7h.jpg",
+    images: [
+     "https://res.cloudinary.com/debewjkqh/image/upload/v1779292975/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_lafh7h.jpg",
      "https://res.cloudinary.com/debewjkqh/image/upload/v1779293223/20260418_131238_jlyx6m.jpg",
     ],
     description: "Movimiento automático, bisel cerámico, resistente al agua.",
