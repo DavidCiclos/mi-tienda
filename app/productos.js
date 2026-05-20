65c7fd2cbf7199d00680b921cc105f97f8de1e83
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-20 11:56:00 -0500

    Update product catalog and fix typos
    Corrected comments and adjusted image URLs formatting. Fixed a typo in the description of the second product.

--- app/productos.js
+++ app/productos.js
@@ -1,4 +1,4 @@
- // productos.js - Catálogo de APEX TIME
+// productos.js - Catálogo de APEX TIME
 export const listaProductos = [
   {
     id: 1,
@@ -7,9 +7,9 @@ export const listaProductos = [
     line: "SUIZOS-S",
     price: 0,
     images: [
-    "https://res.cloudinary.com/debewjkqh/image/upload/v1779291658/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_1_2_ejumav.jpg"
-    "https://res.cloudinary.com/debewjkqh/image/upload/v1779291679/20251129_153935_f3xmcj.jpg"
-     ],        
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1779291658/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_1_2_ejumav.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1779291679/20251129_153935_f3xmcj.jpg"
+    ],        
     description: "Maquinaria suiza, acero 904L, cristal de zafiro.",
     warranty: "2 años"
   },
@@ -20,10 +20,10 @@ export const listaProductos = [
     line: "SUIZOS-S",
     price: 1850000,
     images: [
-     "https://res.cloudinary.com/debewjkqh/image/upload/v1779292975/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_lafh7h.jpg"
-     "https://res.cloudinary.com/debewjkqh/image/upload/v1779293223/20260418_131238_jlyx6m.jpg"
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1779292975/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_lafh7h.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1779293223/20260418_131238_jlyx6m.jpg"
     ],
-    description: "Movimiento automático, bisel cerámico, resistente al agua.",
+    description: "Movimiento automático, bisel cerámico, resistant al agua.",
     warranty: "1 año"
   }
 ];
