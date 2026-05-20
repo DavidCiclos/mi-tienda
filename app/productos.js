c3328a05cff49d8d4a93332b90e803b513f92611
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-20 11:08:07 -0500

    Replace product images for ROLEX SUIZOS-S
    Updated product images for the ROLEX SUIZOS-S.

--- app/productos.js
+++ app/productos.js
@@ -17,8 +17,8 @@ export const listaProductos = [
     line: "SUIZOS-S",
     price: 1850000,
     images: [
-      "https://res.cloudinary.com/debewjkqh/image/upload/v1778263606/nano-banana.jpg",
-     "https://res.cloudinary.com/debewjkqh/image/upload/v1778297784/20260418_131430_vpmq2q.jpg",
+     "https://res.cloudinary.com/debewjkqh/image/upload/v1779292975/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_lafh7h.jpg",
+     "https://res.cloudinary.com/debewjkqh/image/upload/v1779293223/20260418_131238_jlyx6m.jpg",
     ],
     description: "Movimiento automático, bisel cerámico, resistente al agua.",
     warranty: "1 año"
