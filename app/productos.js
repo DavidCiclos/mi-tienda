f7b0b5e281e3c2fd575be023f08ded685ab02dd5
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-20 18:32:18 -0500

    Update product images for SUIZOS-S
    Added a new image URL to the product images array.

--- app/productos.js
+++ app/productos.js
@@ -21,7 +21,8 @@ export const listaProductos = [
     price: 1850000,
     images: [
       "https://res.cloudinary.com/debewjkqh/image/upload/v1779292975/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_2_lafh7h.jpg",
-      "https://res.cloudinary.com/debewjkqh/image/upload/v1779293165/20260418_131228_ntkclr.jpg"
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1779293165/20260418_131228_ntkclr.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1779293164/20260223_105423_vxxtzu.jpg"
     ],
     description: "Movimiento automático, bisel cerámico, resistant al agua.",
     warranty: "1 año"
