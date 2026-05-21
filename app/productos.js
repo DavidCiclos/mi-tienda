77e56dc05e1d5797b96fd122ae0fbe0ba71b2e36
DavidCiclos <ciclosdavid00@gmail.com>
2026-05-20 21:55:47 -0500

    Modify prices and descriptions in productos.js
    Updated product prices and descriptions for several items.

--- app/productos.js
+++ app/productos.js
@@ -5,12 +5,12 @@ export const listaProductos = [
     name: "Daytona S-Clon Gold",
     brand: "ROLEX",
     line: "SUIZOS-S",
-    price: 0,
+    price: 3500000,
     images: [
       "https://res.cloudinary.com/debewjkqh/image/upload/v1779291658/nano-banana-2_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_1_2_ejumav.jpg",
       "https://res.cloudinary.com/debewjkqh/image/upload/v1779291679/20251129_153935_f3xmcj.jpg"
     ],        
-    description: "Maquinaria suiza, acero 904L, cristal de zafiro.",
+    description: "Maquinaria suiza, acero 904L, cristal de zafiro, resitente al agua, logos en ato y bajo relieve, vidrio con logo grabado.",
     warranty: "2 años"
   },
   {
@@ -26,5 +26,61 @@ export const listaProductos = [
     ],
     description: "Movimiento automático, bisel cerámico, resistant al agua.",
     warranty: "1 año"
+  },
+  {
+    id: 3,
+    name: "Skmei supervivencia 1155",
+    brand: "SKMEI",
+    line: "ORIGINALES",
+    price: 119000, // Pon aquí el precio real
+    images: [
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778808365/IMG_20260514_202236_793_hy3wkr.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1779329960/IMG_20260520_211814_402_vfkn1v.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778808365/IMG_20260514_202247_731_cpdukp.jpg"
+    ],
+    description: "Hora analogica, digital, cronometro, alarma y luz, Cuerda militar, Brujula, Pedernal de magnecio.",
+    warranty: "3 meces"
+  },
+  {
+    id: 4,
+    name: "Sanda 6162",
+    brand: "SANDA",
+    line: "ORIGINALES",
+    price: 139000, // Pon aquí el precio real
+    images: [
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778718789/seedream-4.0_hazlo_ver_c%C3%B3mo_presentaci%C3%B3n_de_lujo._fondo_negro_premium_anuncio.-0_isbuhk.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778718789/IMG_20260513_192745_351_cqbkil.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778718789/IMG_20260513_192748_671_bdr3lt.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778718788/IMG_20260513_192753_981_uqqo0i.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778718789/IMG_20260513_192739_367_b7a1pr.jpg"
+    ],
+    description: "A prueba de agua, pulso de silicona en alta resistencia, dual time.",
+    warranty: "Garantia por filtracion de agua y funcionamiento"
+  },
+  {
+    id: 5,
+    name: "Sanda 6008",
+    brand: "SANDA",
+    line: "ORIGINALES",
+    price: 129000, // Pon aquí el precio real
+    images: [
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778808412/IMG_20260514_202342_672_wdcuxy.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778808411/IMG_20260514_202331_328_rvxy4d.jpg",
+      "https://res.cloudinary.com/debewjkqh/image/upload/v1778808410/IMG_20260514_202335_811_wpcekf.jpg"
+    ],
+    description: "Pulso en silicona de lata resistencia, doble hora, incluye estuche.",
+    warranty: "3 meses"
+  },
+  {
+    id: 6,
+    name: "Big Bang Chronograph",
+    brand: "HUBLOT",
+    line: "SUIZOS-S",
+    price: 2100000, // Pon aquí el precio real
+    images: [
+      "TU_URL_DE_CLOUDINARY_AQUÍ"
+    ],
+    description: "Maquinaria suiza cronógrafa funcional, caja de titanio y correa de caucho premium.",
+    warranty: "2 años"
   }
 ];
